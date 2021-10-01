c10eefaead1080c6d06540b0d7c18c674e115be6
Ruvick <rudikov.web@ya.ru>
2021-10-01 19:03:26 +0300

    Добавил файлы json

--- index.php
+++ index.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * The main template file
  *
@@ -20,32 +21,32 @@ get_header();
 <div id="primary" class="content-area">
   <main id="main" class="site-main">
     <?php $arr_slider = carbon_get_theme_option('main_slider');
-      if($arr_slider):
+    if ($arr_slider) :
     ?>
-    <section class="main-slider">
-      <?php foreach($arr_slider as $slide):?>
-        <div class="main-slider__item b-lazy" data-src="<?php echo wp_get_attachment_image_src($slide['main_slider_img'], 'full')[0];?>" style="background-image: url(<?php echo wp_get_attachment_image_src($slide['main_slider_img'], 'full')[0];?>)">
-          <div class="container">
-            <div class="main-slider__item-content">
-              <div class="h1"><?php echo $slide['main_slider_title']?></div>
-              <div class="main-slider__item-content-text">
-                <div class="main-slider__item-content-text-text">
-                  <?php echo $slide['main_slider_text']?>
-                  
+      <section class="main-slider">
+        <?php foreach ($arr_slider as $slide) : ?>
+          <div class="main-slider__item b-lazy" data-src="<?php echo wp_get_attachment_image_src($slide['main_slider_img'], 'full')[0]; ?>" style="background-image: url(<?php echo wp_get_attachment_image_src($slide['main_slider_img'], 'full')[0]; ?>)">
+            <div class="container">
+              <div class="main-slider__item-content">
+                <div class="h1"><?php echo $slide['main_slider_title'] ?></div>
+                <div class="main-slider__item-content-text">
+                  <div class="main-slider__item-content-text-text">
+                    <?php echo $slide['main_slider_text'] ?>
+
+                  </div>
+                  <a href="<?php echo $slide['main_slider_link']; ?>" class="button-green popup-button">Подробнее</a>
                 </div>
-                <a href="<?php echo $slide['main_slider_link'];?>" class="button-green popup-button">Подробнее</a>
+              </div>
+              <div class="main-slider__item-photo">
+                <div class="main-slider__item-bg"></div>
               </div>
             </div>
-            <div class="main-slider__item-photo">
-              <div class="main-slider__item-bg"></div>
-            </div>
           </div>
-        </div>
-      <?php endforeach;?>
-    </section>
-    <?php endif;?>
+        <?php endforeach; ?>
+      </section>
+    <?php endif; ?>
     <!-- <section class="main-slider">
-      <div class="main-slider__item" style="background-image: url(<?php echo get_template_directory_uri();?>/img/vb01.jpg)">
+      <div class="main-slider__item" style="background-image: url(<?php echo get_template_directory_uri(); ?>/img/vb01.jpg)">
         <div class="container">
           <div class="main-slider__item-content">
             <div class="h1">Безболезненная эпиляция</div>
@@ -54,7 +55,7 @@ get_header();
                 на лазерном аппарате Alma Lasers в Центре Лечебной Косметологии
                 
               </div>
-              <a href="<?php echo get_permalink(117);?>" class="button-green popup-button">Подробнее</a>
+              <a href="<?php echo get_permalink(117); ?>" class="button-green popup-button">Подробнее</a>
             </div>
           </div>
           <div class="main-slider__item-photo">
@@ -62,7 +63,7 @@ get_header();
           </div>
         </div>
       </div>
-      <div class="main-slider__item" style="background-image: url(<?php echo get_template_directory_uri();?>/img/vb2.jpg)">
+      <div class="main-slider__item" style="background-image: url(<?php echo get_template_directory_uri(); ?>/img/vb2.jpg)">
         <div class="container">
           <div class="main-slider__item-content">
             
@@ -72,7 +73,7 @@ get_header();
 					
 					СКИДКА 15% <br/>на биоревитализацию 
 				  </div>
-				  <a href="<?php echo get_permalink(14);?>" class="button-green popup-button">Подробнее</a>
+				  <a href="<?php echo get_permalink(14); ?>" class="button-green popup-button">Подробнее</a>
 				</div>
 			
           </div>
@@ -81,7 +82,7 @@ get_header();
           </div>
         </div>
       </div>
-      <div class="main-slider__item" style="background-image: url(<?php echo get_template_directory_uri();?>/img/vb3.jpg)">
+      <div class="main-slider__item" style="background-image: url(<?php echo get_template_directory_uri(); ?>/img/vb3.jpg)">
         <div class="container">
           <div class="main-slider__item-content">
 		  
@@ -90,7 +91,7 @@ get_header();
               <div class="main-slider__item-content-text-text">
                 СКИДКА 20% на Биоревитализацию BlumGel
               </div>
-              <a href="<?php echo get_permalink(14);?>" class="button-green popup-button">Подробнее</a>
+              <a href="<?php echo get_permalink(14); ?>" class="button-green popup-button">Подробнее</a>
             </div>
             
           </div>
@@ -99,7 +100,7 @@ get_header();
           </div>
         </div>
       </div>
-      <div class="main-slider__item" style="background-image: url(<?php echo get_template_directory_uri();?>/img/vb4.jpg)">
+      <div class="main-slider__item" style="background-image: url(<?php echo get_template_directory_uri(); ?>/img/vb4.jpg)">
         <div class="container">
           <div class="main-slider__item-content">
 		  
@@ -110,7 +111,7 @@ get_header();
                 СКИДКА 20% на <br/>Фотоакустическое 4D-омоложение  
              
               </div>
-              <a href="<?php echo get_permalink(14);?>" class="button-green popup-button">Подробнее</a>
+              <a href="<?php echo get_permalink(14); ?>" class="button-green popup-button">Подробнее</a>
             </div>
         
           </div>
@@ -133,7 +134,7 @@ get_header();
                 в Центре Лечебной <br>Косметологии
                 <span class="ttu-bold">Стала еще доступнее!</span>
               </div>
-              <a href="<?php echo get_permalink(14);?>" class="button-green popup-button">Подробнее</a>
+              <a href="<?php echo get_permalink(14); ?>" class="button-green popup-button">Подробнее</a>
             </div>
           </div>
           <div class="main-slider__item-photo">
@@ -144,15 +145,15 @@ get_header();
       </div>
     -->
     <section class="section-pink about">
-      <div class="rose b-lazy" data-src="<?php echo get_template_directory_uri();?>/img/rose.png"></div>
-      <div class="leaf-1 b-lazy" data-src="<?php echo get_template_directory_uri();?>/img/leaf-1.png"></div>
-      <div class="leaf-2 b-lazy" data-src="<?php echo get_template_directory_uri();?>/img/leaf-2.png"></div>
+      <div class="rose b-lazy" data-src="<?php echo get_template_directory_uri(); ?>/img/rose.png"></div>
+      <div class="leaf-1 b-lazy" data-src="<?php echo get_template_directory_uri(); ?>/img/leaf-1.png"></div>
+      <div class="leaf-2 b-lazy" data-src="<?php echo get_template_directory_uri(); ?>/img/leaf-2.png"></div>
       <div class="container">
         <div class="about-wrapper">
-          <div class="about-wrapper__photo b-lazy" data-src="<?php echo get_template_directory_uri();?>/img/main.jpg"></div>
+          <div class="about-wrapper__photo b-lazy" data-src="<?php echo get_template_directory_uri(); ?>/img/main.jpg"></div>
           <div class="about-wrapper__text text_full">
-        <h2 class="section-title "><?php echo carbon_get_theme_option('as_about_main');?></h2>
-          <?php echo wpautop(carbon_get_theme_option('as_about_text'));?>
+            <h2 class="section-title "><?php echo carbon_get_theme_option('as_about_main'); ?></h2>
+            <?php echo wpautop(carbon_get_theme_option('as_about_text')); ?>
           </div>
         </div>
       </div>
@@ -160,27 +161,28 @@ get_header();
     <section class="services section-white">
       <div class="container">
         <h2 class="section-title">Наши услуги</h2>
-        <?php get_template_part('template-parts/services-list');?>
+        <?php get_template_part('template-parts/services-list'); ?>
       </div>
     </section>
 
-	<?php get_template_part('template-parts/consultation');?>
-	
-	<?php get_template_part('template-parts/preim');?>
+    <?php get_template_part('template-parts/consultation'); ?>
 
-	 <?php  
-		require_once __DIR__ . '/Mobile_Detect.php';
-		$detect = new Mobile_Detect;
+    <?php get_template_part('template-parts/preim'); ?>
 
-			if ( !$detect->isMobile() ) {
-			  get_template_part('template-parts/videotur');
-			}
-		?> 
-	  
-    <?php //get_template_part('template-parts/videotur');?>
-    <?php get_template_part('template-parts/reviews');?>
-    <?php get_template_part('template-parts/news');?>
-    <?php get_template_part('template-parts/question-page');?>
+    <?php
+    require_once __DIR__ . '/Mobile_Detect.php';
+    $detect = new Mobile_Detect;
+
+    if (!$detect->isMobile()) {
+      get_template_part('template-parts/videotur');
+    }
+    ?>
+
+    <?php //get_template_part('template-parts/videotur');
+    ?>
+    <?php get_template_part('template-parts/reviews'); ?>
+    <?php get_template_part('template-parts/news'); ?>
+    <?php get_template_part('template-parts/question-page'); ?>
 
   </main><!-- #main -->
 </div><!-- #primary -->
