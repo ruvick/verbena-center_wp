f52195f07185cb0e64580e625685aca05c4833b0
asmi046 <asmi046@gmail.com>
2022-03-18 18:52:19 +0300

    Пиксель Vk

--- header.php
+++ header.php
@@ -71,11 +71,8 @@
 /></noscript>
 <!-- End Facebook Pixel Code -->
 
-
-<script async type="text/javascript">!function(){var t=document.createElement("script");t.type="text/javascript",t.async=!0,t.src="https://vk.com/js/api/openapi.js?169",t.onload=function(){VK.Retargeting.Init("VK-RTRG-1108167-d5ywu"),VK.Retargeting.Hit()},document.head.appendChild(t)}();</script><noscript><img src="https://vk.com/rtrg?p=VK-RTRG-1108167-d5ywu" style="position:fixed; left:-999px;" alt=""/></noscript>
-
 	
-	
+<script type="text/javascript">!function(){var t=document.createElement("script");t.type="text/javascript",t.async=!0,t.src='https://vk.com/js/api/openapi.js?169',t.onload=function(){VK.Retargeting.Init("VK-RTRG-1279623-bdlIB"),VK.Retargeting.Hit()},document.head.appendChild(t)}();</script><noscript><img src="https://vk.com/rtrg?p=VK-RTRG-1279623-bdlIB" style="position:fixed; left:-999px;" alt=""/></noscript>
 	
 	
 </head>
