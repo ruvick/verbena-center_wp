1201e7757e35658de82bcb18aa7cf4b73bc6f28c
asmi046 <asmi046@gmail.com>
2022-04-18 16:09:48 +0300

    Новый пиксель

--- header.php
+++ header.php
@@ -70,15 +70,13 @@
   src="https://www.facebook.com/tr?id=560601938557357&ev=PageView&noscript=1"
 /></noscript>
 <!-- End Facebook Pixel Code -->
-
-	
-<script type="text/javascript">!function(){var t=document.createElement("script");t.type="text/javascript",t.async=!0,t.src='https://vk.com/js/api/openapi.js?169',t.onload=function(){VK.Retargeting.Init("VK-RTRG-1279623-bdlIB"),VK.Retargeting.Hit()},document.head.appendChild(t)}();</script><noscript><img src="https://vk.com/rtrg?p=VK-RTRG-1279623-bdlIB" style="position:fixed; left:-999px;" alt=""/></noscript>
-	
 	
 </head>
 
 <body <?php body_class(); ?>>
-  
+
+  <script type="text/javascript">!function(){var t=document.createElement("script");t.type="text/javascript",t.async=!0,t.src='https://vk.com/js/api/openapi.js?169',t.onload=function(){VK.Retargeting.Init("VK-RTRG-1108167-d5ywu"),VK.Retargeting.Hit()},document.head.appendChild(t)}();</script><noscript><img src="https://vk.com/rtrg?p=VK-RTRG-1108167-d5ywu" style="position:fixed; left:-999px;" alt=""/></noscript>
+
   <div class = "zakrBtn">
 	<a target = "_blank" href = "https://wa.me/+79207195503">
 		<div class = "zbtn wsBtn"></div>
