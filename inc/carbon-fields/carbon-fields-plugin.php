9444c9a091063331b1fb5da88d40de62add9ba25
asmi046 <asmi046@gmail.com>
2022-08-31 12:24:05 +0300

    Новый карбон

--- inc/carbon-fields/carbon-fields-plugin.php
+++ inc/carbon-fields/carbon-fields-plugin.php
@@ -2,13 +2,13 @@
 /**
  * Plugin Name: Carbon Fields
  * Description: WordPress developer-friendly custom fields for post types, taxonomy terms, users, comments, widgets, options, navigation menus and more.
- * Version: 3.0.2
+ * Version: 3.3.4
  * Author: htmlburger
  * Author URI: https://htmlburger.com/
  * Plugin URI: http://carbonfields.net/
  * License: GPL2
- * Requires at least: 4.0
- * Tested up to: 5.0
+ * Requires at least: 5.0
+ * Tested up to: 5.7.1
  * Text Domain: carbon-fields
  * Domain Path: /languages
  */
